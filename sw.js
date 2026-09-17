7eff1fa23626f88da319947c5a4bfab9a54b4262
Vijay-123-co <sun491984@gmail.com>
2026-09-17 11:22:36 +0530

    Update service worker to version 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tet-gyan-v1";
+const CACHE_NAME = "tet-gyan-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -8,32 +8,32 @@ const FILES_TO_CACHE = [
   "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
+    caches.keys().then((keys) =>
       Promise.all(
         keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
       )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
